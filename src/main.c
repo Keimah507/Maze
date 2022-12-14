fd80fb151598860c603ebed8b5be52e7805e609d
keimah507 <githinjibrian5@gmail.com>
2022-12-14 10:41:34 +0300

    Refactoring

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@
 #include "map.h"
 
 const short MAP[MAP_GRID_HEIGHT][MAP_GRID_WIDTH] = {
-	{R, R, R ,R ,R, R, R, R, R, R},
+	{R, R, R, R, R, R, R, R, R, R},
 	{R, B, 0, 0, 0, 0, P, 0, B, R},
 	{R, 0, 0, 0, 0, 0, 0, 0, 0, R},
 	{R, 0, 0, G, 0, 0, G, 0, 0, R},
@@ -21,88 +21,92 @@ const short MAP[MAP_GRID_HEIGHT][MAP_GRID_WIDTH] = {
 };
 
 
-void render(void) 
+void render(void)
 	{
-		if(showMap) 
+		if (showMap)
 		{
 			clearRenderer();
 			renderOverheadMap();
-		} else 
+		} else
 			{
 				renderProjectedScene();
 			}
 	}
 
-void consumeSDLEvents() 
+void consumeSDLEvents(void)
 	{
 	SDL_EVENT event;
 	char KeyIsDown;
 
-	while(SLD_PollEvent(&even)) 
+	while (SDL_PollEvent(&event))
 	{
 		keyIsDown = (event.type == SDL_KEYDOWN);
-		switch(event.type) {
-			case SDL_KEYUP:
-			case SDL_KEYDOWN:
-				switch(event.key.keysym.sym) 
-					{
-						case SDLK_UP:
-							movingForward = keyIsDown;
-							break;
-						case SDLK_DOWN:
-							movingBack = keyIsDown;
-							break;
-						case SDLK_LEFT:
-							turningleft = keyIsDown;
-							break;
-						case SDLK_RIGHT:
-							turningRight = keyIsDown;
-							break;
-						case SDLK_LSHIFT:
-						case SDLK_RSHIFT:
-							playerIsRunning = KeyIsDown;
-							break;
-						case SDLK_ESCAPE:
-							gameIsRunning = FALSE;
-							break;
-						case SDLK_t:
-							if(keyIsDown) textureMode = (textureMode + 1) % 2;
-							break;
-						case SDLK_m:
-							if(keyisDown) showMap = !showMap;
-							break;
-						case SLDK_f:
-							if(keyIsDown) distortion = !distortion;
-							break;
-						case SDLK_r:
-							if (keyIsDown) slowRenderMode = !slowRenderMode;
-							break;
-						case SDLK_c:
-							if (keyIsDown) rayCastMode = (rayCastMode + 1) % 3;
-							break;
-						case SDLK_LEFTBRACKET:
-							if (keyIsDown && distfromViewPlane += 20.0f > 100.0f) distfromViewPlane -= 20.0f);
-							break;
-						case SLDK_RIGHTBRACKET:
-							if (keyIsDown) distfromViewPlane += 20.0f;
-							break;
-						default:
-							break;
-					}
+		switch(event.type)
+		{
+		case SDL_KEYUP:
+		case SDL_KEYDOWN:
+			switch (event.key.keysym.sym) 
+			{
+			case SDLK_UP:
+				movingForward = keyIsDown;
+				break;
+			case SDLK_DOWN:
+				movingBack = keyIsDown;
+				break;
+			case SDLK_LEFT:
+				turningleft = keyIsDown;
+				break;
+			case SDLK_RIGHT:
+				turningRight = keyIsDown;
+				break;
+			case SDLK_LSHIFT:
+			case SDLK_RSHIFT:
+				playerIsRunning = KeyIsDown;
+			break;
+				case SDLK_ESCAPE:
+				gameIsRunning = FALSE;
+				break;
+			case SDLK_t:
+				if (keyIsDown) textureMode =
+					(textureMode + 1) % 2;
+				break;
+			case SDLK_m:
+				if (keyisDown) showMap = !showMap;
 					break;
-					case SDL_QUIT:
-						gameIsRunning = FALSE;
-						break;
-					default:
-						break;
+			case SLDK_f:
+				if(keyIsDown) distortion = !distortion;
+					break;
+			case SDLK_r:
+				if (keyIsDown) slowRenderMode = !slowRenderMode;
+					break;
+			case SDLK_c:
+				if (keyIsDown) rayCastMode =
+					(rayCastMode + 1) % 3;
+					break;
+			case SDLK_LEFTBRACKET:
+				if (keyIsDown && distfromViewPlane += 20.0f >
+						100.0f distfromViewPlane -= 20.0f);
+					break;
+			case SLDK_RIGHTBRACKET:
+				if (keyIsDown) distfromViewPlane += 20.0f;
+					break;
+			default:
+					break;
+				}
+					break;
+			case SDL_QUIT:
+				gameIsRunning = FALSE;
+				break;
+			default:
+			break;
 			}
 		}
 	}
 
-void runGame() 
+void runGame(void) 
 	{
-		long gameTicks = 0;
-		long time;
+	long gameTicks = 0;
+	long time;
 
 	do
 		{
@@ -118,15 +122,15 @@ void runGame()
 
 			SDL_Delay(10);
 
-			if(!(gameTicks++ % 500))
-				fprintf(stderr, "FPS: %.2f\n", 1000.0f / (float)(SDL_GetTicks() - time));
-		}
-		while(gameIsRunning);
+			if (!(gameTicks++ % 500))
+				fprintf(stderr, "FPS: %.2f\n", 1000.0f /
+						(float)(SDL_GetTicks() - time));
+		}while(gameIsRunning);
 	}
 
-int setupWindow() 
+int setupWindow(void)
 	{
-		int x,y;
+		int x, y;
 
 		if (!initGFX("Raycaster", WINDOW_WIDTH, WINDOW_HEIGHT)) return FALSE;
 
@@ -149,32 +153,32 @@ int setupWindow()
 		return TRUE;
 	}
 
-int main() 
+int main(void) 
 	{
-		Uint32 *screenBuffer = NULL;
-		Uint32 *redXorTexture = NULL;
-		Uint32 *greenXorTexture = NULL;
-		Uint32 *blueXorTexture = NULL;
-		Uint32 *grayXorTexture = NULL;
+	Uint32 *screenBuffer = NULL;
+	Uint32 *redXorTexture = NULL;
+	Uint32 *greenXorTexture = NULL;
+	Uint32 *blueXorTexture = NULL;
+	Uint32 *grayXorTexture = NULL;
 
-		const Uint32 COLORS[4] =
-		{
-        		RGBtoABGR(255, 0, 0),
-        		RGBtoABGR(0, 255, 0),
-        		RGBtoABGR(0, 0, 255),
-        		RGBtoABGR(128, 128, 128)
-		};
+	const Uint32 COLORS[4] =
+	{
+        	RGBtoABGR(255, 0, 0),
+        	RGBtoABGR(0, 255, 0),
+        	RGBtoABGR(0, 0, 255),
+        	RGBtoABGR(128, 128, 128)
+	};
 
-		Uint32 *TEXTURES[4];
+	Uint32 *TEXTURES[4];
 
-		char gameIsRunning = TRUE;
-		char showMap = TRUE;
-		char distortion = FALSE;
-		char slowRenderMode = FALSE;
-		char RayCastMode = 0;
-		char textureMode = 0;
+	char gameIsRunning = TRUE;
+	char showMap = TRUE;
+	char distortion = FALSE;
+	char slowRenderMode = FALSE;
+	char RayCastMode = 0;
+	char textureMode = 0;
 
-	if(!setupWindow())
+	if (!setupWindow())
 	{
 		fprintf(stderr, "Could not Initialize raycaster!\n");
 		return EXIT_FAILURE;
